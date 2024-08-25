e4633ba645cfa8677a3275bca2213533da4b6963
souradipdev <22052939@kiit.ac.in>
2024-08-26 02:44:52 +0530

    Assigned required names

---
+++ src/name.ts
@@ -0,0 +1,7 @@
+export const db = "stack_overflow";
+export const questionCollection = "questions";
+export const answerCollection = "answers";
+export const commentCollection = "comments";
+export const voteCollection = "votes";
+
+export const questionAttachmentBucket = "question-attachment";
