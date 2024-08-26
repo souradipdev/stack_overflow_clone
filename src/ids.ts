da2f346a570fcd69b995a245cfe4039ea0997279
souradipdev <22052939@kiit.ac.in>
2024-08-27 00:36:30 +0530

    Assigned required individual ids

---
+++ src/ids.ts
@@ -0,0 +1,9 @@
+import {nanoid} from "nanoid";
+
+export const dbID = nanoid();
+export const questionCollectionID = nanoid();
+export const answerCollectionID = nanoid();
+export const commentCollectionID = nanoid();
+export const voteCollection = nanoid();
+
+export const questionAttachmentBucketID = nanoid();
