317274da23b7399f748058209c66db5cab79ff0a
souradipdev <22052939@kiit.ac.in>
2024-08-27 01:34:53 +0530

    Created server comment collection

---
+++ src/models/server/comment.collection.ts
@@ -0,0 +1,28 @@
+import {IndexType, Permission} from "node-appwrite";
+import {db, commentCollection} from "@/name";
+import {dbID, commentCollectionID, questionCollectionID} from "@/ids";
+import {databases} from "@/models/server/config";
+
+export default async function createCommentCollection() {
+
+  await databases.createCollection(dbID, commentCollectionID, commentCollection, [
+    Permission.read("any"),
+    Permission.read("users"),
+    Permission.create("users"),
+    Permission.delete("users"),
+    Permission.update("users"),
+    Permission.write("users")
+  ]);
+
+  console.log("Comment collection created");
+
+  await Promise.all([
+    databases.createStringAttribute(dbID, questionCollectionID, "content", 10000, true),
+    databases.createEnumAttribute(dbID,
+      questionCollectionID, "typeId", ["answer", "question"], true),
+    databases.createStringAttribute(dbID, questionCollectionID, "typeId", 50, true),
+    databases.createStringAttribute(dbID, questionCollectionID, "authorId", 50, true, undefined, true)
+  ]);
+
+  console.log("Comment collection attributes created");
+}
